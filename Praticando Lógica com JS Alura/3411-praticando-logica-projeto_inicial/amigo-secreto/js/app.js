29298b0b0f2a01927b6fc38c3397f2a7942994e7
UmVitorAleatorio <vitorhiper@hotmail.com>
2024-06-26 21:15:49 -0300

    Amigo secreto funcional

---
+++ Praticando Lógica com JS Alura/3411-praticando-logica-projeto_inicial/amigo-secreto/js/app.js
@@ -0,0 +1,42 @@
+let amigos = [];
+
+function adicionar(){
+    let amigo = document.getElementById('nome-amigo');
+    let lista = document.getElementById('lista-amigos');
+    amigos.push(amigo.value);
+
+    if(lista.textContent == ''){
+        lista.textContent = amigo.value;
+    }else{
+        lista.textContent = lista.textContent + ', ' + amigo.value;
+    }
+    amigo.value = '';
+}
+
+function sortear(){
+    embaralha(amigos);
+    let listaSorteio = document.getElementById('lista-sorteio');
+
+    for( let i = 0; i < amigos.length; i++){
+        if(i == amigos.length - 1){
+            listaSorteio.innerHTML = listaSorteio.innerHTML + amigos[0]+ ' --> ' + amigos[i+0] + '<br>'
+        }else{
+            listaSorteio.innerHTML = listaSorteio.innerHTML + amigos[i]+ ' --> ' + amigos[i+1] + '<br>'
+        }
+    }
+}
+
+function embaralha(lista){
+    for (let i = lista.length; i; i--){
+        const iAleatorio = Math.floor(Math.random()*i);
+
+        [lista[i - 1], lista[iAleatorio]] =
+             [lista[iAleatorio], lista[i-1]];
+    }
+}
+
+function reiniciar(){
+    amigos  = [];
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
+}
