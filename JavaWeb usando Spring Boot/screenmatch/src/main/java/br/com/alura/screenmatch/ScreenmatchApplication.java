c3ad9f6aaf36b955fbabbe89fcc647eed5efab95
UmVitorAleatorio <vitorhiper@hotmail.com>
2024-07-11 13:34:48 -0300

    Projeto usando Spring sem web, finalização do primeiro curso de Spring

--- JavaWeb usando Spring Boot/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ JavaWeb usando Spring Boot/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,8 +1,6 @@
 package br.com.alura.screenmatch;
 
-import br.com.alura.screenmatch.model.DadosSerie;
-import br.com.alura.screenmatch.service.ConsumoApi;
-import br.com.alura.screenmatch.service.ConverteDados;
+import br.com.alura.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,13 +14,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-		var json =consumoApi.obterDados("https://www.omdbapi.com/?t=supernatural&apikey=c2c117c2");
-//		System.out.println(json);
-//		json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
-		System.out.println(json);
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 }
