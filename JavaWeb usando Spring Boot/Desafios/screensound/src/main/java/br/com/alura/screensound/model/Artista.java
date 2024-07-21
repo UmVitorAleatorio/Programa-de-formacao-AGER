bfd8e21924b4b45688e5476245a4bdc2ba9d8a66
UmVitorAleatorio <vitorhiper@hotmail.com>
2024-07-20 21:06:17 -0300

    Finalizando o projeto do ScreenSound

---
+++ JavaWeb usando Spring Boot/Desafios/screensound/src/main/java/br/com/alura/screensound/model/Artista.java
@@ -0,0 +1,68 @@
+package br.com.alura.screensound.model;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "artistas")
+public class Artista {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(unique = true)
+    private String nome;
+    @Enumerated(EnumType.STRING)
+    private TipoArtista tipo;
+
+    @OneToMany(mappedBy = "artista", cascade = CascadeType.ALL, fetch = FetchType.EAGER )
+    private List<Musica> musicas = new ArrayList<>();
+
+    public Artista() {
+    }
+
+    @Override
+    public String toString() {
+        return  "Artista = " + nome +
+                ", Tipo = " + tipo +
+                ", Músicas = " + musicas;
+    }
+
+    public Artista(String nome, TipoArtista tipo) {
+        this.nome = nome;
+        this.tipo = tipo;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public TipoArtista getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoArtista tipo) {
+        this.tipo = tipo;
+    }
+
+    public List<Musica> getMusicas() {
+        return musicas;
+    }
+
+    public void setMusicas(List<Musica> musicas) {
+        this.musicas = musicas;
+    }
+}
---
+++ JavaWeb usando Spring Boot/Desafios/screensound/src/main/java/br/com/alura/screensound/model/Musica.java
@@ -0,0 +1,53 @@
+package br.com.alura.screensound.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "musicas")
+public class Musica {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String titulo;
+
+    @ManyToOne
+    private Artista artista;
+
+    public Musica(String nomeMusica) {
+        this.titulo = nomeMusica;
+    }
+
+    public Musica() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Artista getArtista() {
+        return artista;
+    }
+
+    public void setArtista(Artista artista) {
+        this.artista = artista;
+    }
+
+    @Override
+    public String toString() {
+        return "Música = " + titulo +
+                ", Artista = " + artista.getNome();
+    }
+}
